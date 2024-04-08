45090b8a702ce337eb3f90955da07dc470112e3f
benten <ben@benten.ca>
2024-04-07 21:37:25 -0600

    add comments

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import vono from "@vonojs/vono"
 import Adaptor from "@vonojs/vono/adaptor-node"
 import * as fs from "node:fs/promises";
 
+// Hack to include the server entry in the final build
+// https://github.com/brillout/vite-plugin-server-entry?tab=readme-ov-file#manual-import
 const adaptor = new Adaptor()
 const oldBuildEnd = adaptor.buildEnd
 adaptor.buildEnd = async () => {
@@ -13,7 +15,10 @@ adaptor.buildEnd = async () => {
   await fs.writeFile("dist/server/run.js", `import "./entry.mjs";\n${file}`)
 }
 
-// Ugly hack to prevent Vike from creating middleware
+/*  hack to prevent Vike from creating middleware
+ Otherwise it overrides vono's middleware
+ todo: look into handling static asset serving with vono in dev which would override plugins that use middleware
+ */
 const toggleMiddlewareMode = (toggle: boolean): Plugin => {
   let config: any;
   return {
